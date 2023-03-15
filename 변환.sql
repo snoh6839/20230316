9d659bb26211d74cd8180fb2e660dcabc7fc4850
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-15 17:05:24 +0900

    if, if null

--- 변환.sql
+++ 변환.sql
@@ -1,1 +1,7 @@
-SELECT CAST (1234 AS CHAR(4));
+-- SELECT if( 1 < 2 , '참', '거짓');
+
+-- SELECT emp_no, if( emp_no = 10001 , first_name, birth_date )
+-- FROM employees;
+
+-- SELECT IFNULL(NULL,'aa');
+-- SELECT IFNULL('123','aa');
